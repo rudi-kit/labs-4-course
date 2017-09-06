2bbf21d509d47a554015c023533f5c63bba934f3
Rizhi-KotE <d1mqn1ch@mail.ru>
2017-09-06 08:35:57 +0300

    draw pixel

--- src/labs_4_cource/core.cljs
+++ src/labs_4_cource/core.cljs
@@ -1,17 +1,63 @@
 (ns labs-4-cource.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+   [reagent.core :as reagent]))
 
-(defonce example (.getElementById js/document "example"))
+(def window-width (reagent/atom nil))
 
-(defonce ctx (.getContext example "2d"))
+(defn on-window-resize [evt]
+  (reset! window-width (.-innerWidth js/window)))
 
-(defn draw-canvas []
-    (.fillRect ctx 0 0 (.-width example) (.-height example))
-    )
+(defn pdebug [value]
+  (println value)
+  value)
 
-(defn init! []
-  (draw-canvas))
+(defn draw-pixel [node [x y]]
+  (let [ctx (-> node
+                .-children
+                (aget 0)
+                (.getContext "2d"))
+        ]
+      (-> ctx
+          (.fillRect x y 1 1))))
 
-(init!)
+(defn draw-canvas-contents [canvas]
+  (let [ctx (.getContext canvas "2d")
+        w (.-clientWidth canvas)
+        h (.-clientHeight canvas)]
+    (.beginPath ctx)
+    (.moveTo ctx 0 0)
+    (.lineTo ctx w h)
+    (.moveTo ctx w 0)
+    (.lineTo ctx 0 h)
+    (.stroke ctx)))
 
+(defonce dom-node (reagent/atom nil))
+
+(defn div-with-canvas []
+  (reagent/create-class
+   {:component-did-update
+    (fn [this]
+      (draw-canvas-contents (.-firstChild @dom-node)))
+
+    :component-did-mount
+    (fn [this]
+      (reset! dom-node (reagent/dom-node this)))
+
+    :reagent-render
+    (fn []
+      @window-width ;; Trigger re-render on window resizes
+      [:div.with-canvas
+         ;; reagent-render is called before the compoment mounts, so
+         ;; protect against the null dom-node that occurs on the first
+         ;; render
+       [:canvas (if-let [node @dom-node]
+                  {:width (.-clientWidth node)
+                   :height (.-clientHeight node)})]])}))
+
+(defn home []
+  [div-with-canvas])
+
+(defn ^:export init! []
+  (reagent/render [home]
+                  (.getElementById js/document "app"))
+  (.addEventListener js/window "resize" on-window-resize))
