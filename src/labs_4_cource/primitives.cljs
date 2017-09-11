951faa0613d27342ad350f21688044a53d2a422d
Dzmitry Brutski <dbrutski@iba.by>
2017-09-11 13:38:22 +0300

    brezenhame works with 2nd quadrant

--- src/labs_4_cource/primitives.cljs
+++ src/labs_4_cource/primitives.cljs
@@ -15,27 +15,33 @@
               [x2 y2] p2
               length (max (Math/abs (- x2 x1)) (Math/abs (- y2 y1)))
               dx (/ (- x2 x1) length)
-              dy (/ (- y2 y1) length)]
+              dy (/ (- y2 y1) length)
+              ]
             (->> [x1 y1 0]
                  (iterate (fn [[x y i]] [(+ x dx) (+ y dy) (+ i 1)]))
                  (take-while (fn [[x y i]] (<= i length)))
                  (map (fn [[x y]] [ (floor x) (floor y)]))))))
+
+(defn calc-steps [coordinate]
+    (mapv Math/sign coordinate)  )
 
 (deftype BrezenhameLine [p1 p2]
     Line
     (line-points [this]
         (let [[x1 y1] p1
               [x2 y2] p2
-              dx (- x2 x1)
-              dy (- y2 y1)
-              e (- (* 2 dy) dx)]
+              dx  (- x2 x1)
+              dy  (- y2 y1)
+              e (- (* 2 dy) (Math/abs dx))
+              [x-step y-step] (calc-steps [dx dy])
+              ]
             (->>  [x1 y1 e]
                   (iterate (fn [[x y e]]
-                               (if (>= e 0) [(+ 1 x) (+ 1 y) (+ e (* 2 dy) (- (* 2 dx)))]
-                                   [(+ 1 x) y (+ e (* 2 dy))])))
-                  (take dx)
+                               (if (>= e 0) [(+ x-step x) (+ y-step y) (+ e (* 2 dy) (- (* 2 (Math/abs dx))))]
+                                   [(+ x-step x) y (+ e (* 2 dy))])))
+                  (take (+ 1 (Math/abs dx)))
                   (map (fn [[x y]] [x y]))
-                 ))))
+                  ))))
 
 (deftest line-from-book
     (are [src expected] (= src expected)
@@ -48,3 +54,10 @@
         (line-points (BrezenhameLine. [0 0] [9 4])) [[0 0] [1 0] [2 1] [3 1] [4 2] [5 2] [6 3] [7 3] [8 4] [9 4]]
         (line-points (BrezenhameLine.  [5 0] [0 1])) [[5 0] [4 0] [3 0] [2 1] [1 1] [0 1]]))
 (cljs.test/run-tests)
+
+(deftest steps
+    (are [src expected] (= (calc-steps src) expected)
+        [1 1] [1 1]
+        [1 -1] [1 -1]
+        [-1 1] [-1 1]
+        [-1 -1] [-1 -1]))
