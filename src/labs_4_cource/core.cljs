b15e9448b1278e3f1e9a946299ab24d80c1ea201
Rizhi-KotE <d1mqn1ch@mail.ru>
2017-09-11 23:30:42 +0300

    toggles

--- src/labs_4_cource/core.cljs
+++ src/labs_4_cource/core.cljs
@@ -2,8 +2,22 @@
   (:require [labs-4-cource.canvas-component :refer [div-with-canvas]]
             [reagent.core :as reagent]))
 
+(defn toggles [selected values on-change]
+    [:div
+     (doall (for [value values]
+                ^{:key value}
+                [:label 
+                 [:input {:type "radio"
+                          :checked (= @selected value)
+                          :onClick (partial on-change value)}]
+                 value]))])
+
+(defonce selected (reagent/atom :simple))
+(defn change-selected [value] (reset! selected value))
 (defn home []
-  [div-with-canvas])
+  [:div
+   [div-with-canvas]
+   [toggles selected [:simple :be :smooth] change-selected]])
 
 (defn ^:export init! []
   (reagent/render [home]
