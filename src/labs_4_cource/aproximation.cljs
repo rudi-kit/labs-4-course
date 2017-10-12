64efc7503569b23cbe627296c6bbf5e36d32796c
Rizhi-KotE <d1mqn1ch@mail.ru>
2017-10-13 01:03:42 +0300

    linearize

--- src/labs_4_cource/aproximation.cljs
+++ src/labs_4_cource/aproximation.cljs
@@ -1,6 +1,25 @@
 (ns labs-4-cource.aproximation
   (:require [clojure.core.matrix :as m]
-            [labs-4-cource.first-order-lines :refer [floor line-points]]))
+            [labs-4-cource.first-order-lines
+             :refer
+             [->BrezenhameLine floor line-points]]))
+
+(defn get-form-points [matrix [p1 p2 p3 p4]]
+    (m/emap
+     floor
+     (m/mmul
+      (m/matrix
+       (map (fn [t] [(* t t t) (* t t) t 1]) (range 0 1 0.05)))
+      matrix
+      (m/matrix (list p1
+                      p2
+                      p3
+                      p4)))))
+
+(defn linearise [points]
+    (mapcat line-points
+            (map (fn [[p1 p2]] (->BrezenhameLine p1 p2))
+                 (partition 2 1 points))))
 
 (defn ->Ermit [p1 p2 p3 p4]
   (let [[x1 y1] p1
@@ -17,16 +36,8 @@
     [1  0  0  0]]))
 
 (defmethod line-points :ermit [{:keys [p1 p4 v1 v4]}]
-  (m/emap
-   floor
-   (m/mmul
-    (m/matrix
-     (map (fn [t] [(* t t t) (* t t) t 1]) (range 0 1 0.01)))
-    ermit-matrix
-    (m/matrix (list p1
-                    p4
-                    v1
-                    v4)))))
+    (linearise (get-form-points ermit-matrix [p1 p4 v1 v4])))
+
 (line-points (->Ermit [30 30] [240 300] [130 20] [200 400]))
 
 (defn ->Bezie [p1 p2 p3 p4]
@@ -40,22 +51,12 @@
     [1  0  0 0]]))
 
 (defmethod line-points :bezie [{:keys [p1 p2 p3 p4]}]
-  (m/emap
-   floor
-   (m/mmul
-    (m/matrix
-     (map (fn [t] [(* t t t) (* t t) t 1]) (range 0 1 0.01)))
-    bezie-matrix
-    (m/matrix (list p1
-                    p2
-                    p3
-                    p4)))))
+    (linearise (get-form-points bezie-matrix [p1 p2 p3 p4])))
 
 (defn ->Spline [& rest]
-  {:type :spline :points (into [] (concat (list (first rest)) rest (take-last 1 rest)))})
-
-(comment
-  (->Spline [0 0] [100 100]))
+  (let [f (take 1 rest)
+        l (take-last 1 rest)]
+    {:type :spline :points (into [] (concat f f rest l l))}))
 
 (def b-spline-matrix
   (m/mul
@@ -67,20 +68,12 @@
    (/ 1 6)))
 
 (defn b-spline-segment [[p1 p2 p3 p4]]
-  (m/emap
-   floor
-   (m/mmul
-    (m/matrix
-     (map (fn [t] [(* t t t) (* t t) t 1]) (range 0 1 0.01)))
-    b-spline-matrix
-    (m/matrix (list p1
-                    p2
-                    p3
-                    p4)))))
+  (get-form-points b-spline-matrix [p1 p2 p3 p4]))
 
 (defmethod line-points :spline [{points :points}]
-  (mapcat b-spline-segment  (partition 4 1 points)))
+  (linearise (mapcat b-spline-segment (partition 4 1 points))))
 
 (comment
-  (line-points {:type :spline :points [[0 0] [100 100] [200 200] [100 100]]})
-  (b-spline-segment [[0 0] [100 100] [200 200] [100 100]]))
+  (line-points {:type :spline :points [[0 0] [0 0] [100 100] [200 200] [100 100][100 100]]})
+  (doall
+   (partition 2 1 (b-spline-segment [[0 0] [100 100] [200 200] [100 100]]))))
